8350480978a78a01a1f62c278872994a977411cf
PZYCO YT <64959978+PZYCO20@users.noreply.github.com>
2021-12-11 23:45:49 +0800

    physics engine can now jump

--- Jump physics test/index.js
+++ Jump physics test/index.js
@@ -7,7 +7,7 @@ let lastFrame = 0;
 let width = 800;
 let height = 400;
 let friction = 0.8;
-let gravity = 0.3;
+let gravity = 1;
 let fps;
 
 // executes when page is loaded
@@ -36,25 +36,24 @@ function gameLoop(currentTime) {
 
   // Pass the delta time to the update
   // Game logic here
-  update(delta);
+  update();
   render();
   // console.log(`Elapsed: ${elapsedTime}`);
 
-  if (elapsedTime < 20) {
+  if (false) {
     requestAnimationFrame(gameLoop);
   }
 }
 
 // class data
 class Player {
-  constructor(x, y, width, height, speed) {
+  constructor(x, y, width, height, movementSpeed) {
     this.x = x;
     this.y = y;
+    this.velocityX = movementSpeed;
+    this.velocityY = movementSpeed;
     this.width = width;
     this.height = height;
-    this.speed = speed;
-    this.velocityX = 0;
-    this.velocityY = 0;
     this.isJumping = false;
     this.isGrounded = false;
   }
@@ -92,40 +91,48 @@ addEventListener('keydown', () => {
   switch (event.key) {
     case 'ArrowUp':
       //up
-      player_y -= 5;
+      player_y -= 50;
 
       // console.log('up');
       break;
     case 'ArrowDown':
       //down
-      player_y += 5;
+      player_y += 50;
       // console.log('down');
       break;
     case 'ArrowLeft':
       //left
-      player_x -= 5;
+      player_x -= 50;
 
       // console.log('left');
       break;
     case 'ArrowRight':
       //right
-      player_x += 5;
+      player_x += 50;
       // console.log('right');
       break;
   }
 });
 
-function update(delta) {
+// physics
+function update() {
   //entity creation
-  player = new Player(player_x - 16, player_y - 16, 32, 32, 3 * delta);
+  player = new Player(player_x - 16, player_y - 16, 32, 32, 2);
 
-  // Player.velX *= friction;
-  // Player.velY += gravity;
+  // floor detection
+  if (player.y + player.height > canvas.height) {
+    player.velocityY = 0;
+  } else {
+    player.velocityX *= friction;
+    player.velocityY += gravity;
+  }
 
-  // player_x += Player.velX;
-  // player_y += Player.velY;
+  // moves the player
+  // player_x += player.velocityX;
+  player_y += player.velocityY;
 }
 
+// implementation
 function render() {
   // Clear the entire canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
